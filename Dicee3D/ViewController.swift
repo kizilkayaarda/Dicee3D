87da1061b414384de2091f8e1b7138c8e45e8715
Cemal Arda KIZILKAYA <cemalardakizilkaya@cemal-macbook-pro.local>
2018-12-01 23:55:49 +0300

    Create the dice object as a cube using SCNBox and place it in the real world

--- Dicee3D/ViewController.swift
+++ Dicee3D/ViewController.swift
@@ -20,14 +20,20 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
+        // creating the dice object
+        let cube = SCNBox(width: 0.15, height: 0.15, length: 0.15, chamferRadius: 0.02)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        cube.materials = [material]
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        // positioning the dice object in the real world
+        let node = SCNNode()
+        node.position = SCNVector3(0, 1, -0.5)
+        node.geometry = cube
         
-        // Set the scene to the view
-        sceneView.scene = scene
+        // adding dice obejct to the scene
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -45,31 +51,5 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Pause the view's session
         sceneView.session.pause()
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
