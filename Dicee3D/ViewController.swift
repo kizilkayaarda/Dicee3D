1a83c0991bd14a4678125c3b7ee379767fee02e3
Cemal Arda KIZILKAYA <cemalardakizilkaya@cemal-macbook-pro.local>
2018-12-03 23:17:54 +0300

    Add dice rolling functionality as well as an option to remove all dice on the scene

--- Dicee3D/ViewController.swift
+++ Dicee3D/ViewController.swift
@@ -12,6 +12,9 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    // to store the dice that are added to the scene
+    var diceArray = [SCNNode]()
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
@@ -61,13 +64,53 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                         y: hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
                         z: hitResult.worldTransform.columns.3.z
                     )
- 
+                    
+                    diceArray.append(diceNode)
                     sceneView.scene.rootNode.addChildNode(diceNode)
-             }
+                    
+                    rollDice(dice: diceNode)
+                }
+            }
                 
+        }
+    }
+    
+    // MARK: - Functions for dice rolls
+    func rollAllOnTheSurface() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                rollDice(dice: dice)
             }
         }
     }
+    
+    func rollDice(dice: SCNNode) {
+        
+        // generating random angles for rolling animation
+        let randX = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
+        let randZ = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
+        
+        dice.runAction(SCNAction.rotateBy(x: CGFloat(randX * 5), y: 0, z: CGFloat(randZ * 5), duration: 0.5))
+        
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAllOnTheSurface()
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        rollAllOnTheSurface()
+    }
+    
+    // MARK: - Function for removing all dice on the scene
+    @IBAction func removeAll(_ sender: UIBarButtonItem) {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                dice.removeFromParentNode()
+            }
+        }
+    }
+    
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         if anchor is ARPlaneAnchor {
